d0652992df2f1d8e8cb371f8b24905726d99519f
WangCheng23 <cheng.wang523@outlook.com>
2023-07-20 22:20:42 +0800

    update

--- visionProject/Belt_deviation_baseline/src_repo/start_train.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /project/train/src_repo
-rm -rf /project/train/models/*
-pip install -r requirements.txt -i https://mirrors.aliyun.com/pypi/simple
-
-python scripts/convert_tusimple.py --data_root=/home/data/67
-
-python train.py configs/tusimple.py --data_root=/home/data/67 --epoch=10 --batch_size=32
